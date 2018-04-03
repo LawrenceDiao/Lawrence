5221534942946f023d1f830f2a02df8b2b8a0311
Lawrence Diao <37821615+LawrenceDiao@users.noreply.github.com>
2018-04-03 09:36:24 +0800

    Add files via upload

---
+++ rw_visual.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+while True:
+# 创建一个RandomWalk
+    rw = RandomWalk(50000)
+    rw.fill_walk()
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values,rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
+
+    # 突出起点和终点
+    plt.scatter(0,0, c='green' ,edgecolors='none', s=1)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red',edgecolors='none', s=1)
+    # 隐藏坐标轴
+
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+    # plt.scatter(rw.x_values, rw.y_values, s=15)
+
+    # 调整分辨率
+
+    plt.figure(dpi=128, figsize=(10,6))
+
+    plt.show()
+    keep_running = input("Make another walk (y/n): ")
+    if keep_running == 'n':
+        break
+
